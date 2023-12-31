b28810ad2837bcd314af67350c56fc8946d53ae9
sobri <slier81@gmail.com>
2023-12-31 10:35:43 +0800

    Finish Challenge#5

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -20,24 +20,31 @@ package main
 import (
 	"errors"
 	"log"
+	"sync"
+	"time"
 )
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
 	sessions map[string]Session
+	mutex    sync.RWMutex
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data       map[string]interface{}
+	LastUpdate time.Time
 }
 
 // NewSessionManager creates a new sessionManager
 func NewSessionManager() *SessionManager {
 	m := &SessionManager{
 		sessions: make(map[string]Session),
+		mutex:    sync.RWMutex{},
 	}
+
+	m.CleanSession()
 
 	return m
 }
@@ -45,12 +52,17 @@ func NewSessionManager() *SessionManager {
 // CreateSession creates a new session and returns the sessionID
 func (m *SessionManager) CreateSession() (string, error) {
 	sessionID, err := MakeSessionID()
+
 	if err != nil {
 		return "", err
 	}
 
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
 	m.sessions[sessionID] = Session{
-		Data: make(map[string]interface{}),
+		Data:       make(map[string]interface{}),
+		LastUpdate: time.Now(),
 	}
 
 	return sessionID, nil
@@ -63,32 +75,64 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
 	session, ok := m.sessions[sessionID]
+
 	if !ok {
 		return nil, ErrSessionNotFound
 	}
+
 	return session.Data, nil
 }
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
 	_, ok := m.sessions[sessionID]
+
 	if !ok {
 		return ErrSessionNotFound
 	}
 
 	// Hint: you should renew expiry of the session here
 	m.sessions[sessionID] = Session{
-		Data: data,
+		Data:       data,
+		LastUpdate: time.Now(),
 	}
 
 	return nil
 }
 
+func (m *SessionManager) CleanSession() {
+	go func() {
+		for {
+			m.mutex.Lock()
+
+			for key := range m.sessions {
+				now := time.Now()
+				lastUpdate := m.sessions[key].LastUpdate
+				timeDifferences := now.Sub(lastUpdate)
+
+				if timeDifferences.Seconds() > 5 {
+					delete(m.sessions, key)
+				}
+			}
+
+			m.mutex.Unlock()
+			time.Sleep(time.Millisecond * 300)
+		}
+	}()
+}
+
 func main() {
 	// Create new sessionManager and new session
-	m := NewSessionManager()
-	sID, err := m.CreateSession()
+	sm := NewSessionManager()
+	sID, err := sm.CreateSession()
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -99,15 +143,17 @@ func main() {
 	data := make(map[string]interface{})
 	data["website"] = "longhoang.de"
 
-	err = m.UpdateSessionData(sID, data)
+	err = sm.UpdateSessionData(sID, data)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Update session data, set website to longhoang.de")
+	log.Println("Updating session data, set website to longhoang.de")
 
 	// Retrieve data from manager again
-	updatedData, err := m.GetSessionData(sID)
+	updatedData, err := sm.GetSessionData(sID)
+
 	if err != nil {
 		log.Fatal(err)
 	}
